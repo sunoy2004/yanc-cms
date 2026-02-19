abfbfc9ffa92a306af62824e1f7ab9a0fd60186c
Sunoy <128156945+sunoy2004@users.noreply.github.com>
2026-02-19 14:00:49 +0530

    Add localhost:8080 to CORS allowed origins

--- apps/cms-api/src/main.ts
+++ apps/cms-api/src/main.ts
@@ -71,6 +71,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:5173',
       'http://127.0.0.1:5173',
+      'http://localhost:8080',
       'http://127.0.0.1:3000'
     ];
 
